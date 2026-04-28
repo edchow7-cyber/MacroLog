dceeafd2ef15f560f1f359458f48f9d6c2b4d015
edchow7-cyber <edchow7@gmail.com>
2026-04-28 16:03:00 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// MacroLog Service Worker
+// Bump VERSION with every release to force iOS to reload
+const VERSION = 'v1.0.1';
+const CACHE = `macrolog-${VERSION}`;
+
+// Files to cache for offline use
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Always fetch fresh from network, fall back to cache
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
